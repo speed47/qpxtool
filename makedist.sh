cc3dedbd44d7de31edd1ddd6395095cdbd895502
Stéphane Lesimple <speed47_github@speed47.net>
2020-09-17 22:02:01 +0200

    add makedist.sh

---
+++ makedist.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -e
+cd "$(dirname "$0")"
+rm -rf dist
+mkdir dist
+for i in $(ntldd -R gui/release/qpxtool  | awk '/mingw/ {print $3}' | tr \\\\ / | grep -Eo '[^/]+$'); do cp -va $MINGW_PREFIX/bin/$i dist/; done
+cp -va gui/release/qpxtool.exe console/*/*.exe dist/
+find lib/lib -name "*.dll" | xargs cp -vat dist
+mkdir -p dist/share/qt5/plugins/platforms
+cp -va $MINGW_PREFIX/share/qt5/plugins/platforms/* dist/share/qt5/plugins/platforms/
+mkdir -p dist/plugins
+cp -va plugins/lib/libqscan_*.dll dist/plugins/
