b4617b49f8deb270edaa693a9554fd3945f37eee
Stéphane Lesimple <speed47_github@speed47.net>
2020-09-20 11:19:50 +0200

    chore: fix makedist when we have both release and debug verssions

--- makedist.sh
+++ makedist.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-set -e
+if [ ! -e "gui/release/qpxtool.exe" ] && [ -e "gui/debug/qpxtool.exe" ]; then
+	flavor=debug
+else
+	flavor=release
+fi
 cd "$(dirname "$0")"
 rm -rf dist
 mkdir dist
-for i in $(ntldd -R gui/*/qpxtool.exe  | awk '/mingw/ {print $3}' | tr \\\\ / | grep -Eo '[^/]+$'); do cp -va $MINGW_PREFIX/bin/$i dist/; done
-cp -va gui/*/qpxtool.exe console/*/*.exe dist/
+for i in $(ntldd -R gui/$flavor/qpxtool.exe  | awk '/mingw/ {print $3}' | tr \\\\ / | grep -Eo '[^/]+$'); do cp -va $MINGW_PREFIX/bin/$i dist/; done
+cp -va gui/$flavor/qpxtool.exe console/*/*.exe dist/
 find lib/lib -name "*.dll" | xargs cp -vat dist
 mkdir -p dist/share/qt5/plugins/platforms
 cp -va $MINGW_PREFIX/share/qt5/plugins/platforms/* dist/share/qt5/plugins/platforms/
