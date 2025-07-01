afbad27039b4a746e39526dff24e96089a785661
Stéphane Lesimple <speed47_github@speed47.net>
2025-07-01 18:18:13 +0200

    fix: compilation errors under macOS

--- gui/src/mainwindow.cpp
+++ gui/src/mainwindow.cpp
@@ -1399,7 +1399,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		idir = "images";
 		r+= "\t<img src=\":images/logo.png\">\n";
 	} else {
-		r+= "\t<img src=\""+idir+"/logo.png\">\n";
+		r+= "\t<img src=\"" + idir + "/logo.png\">\n";
 	}
 
 	r+= "\t<br>\n";
@@ -1453,7 +1453,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 
 	if (dev->testData.rt.size() > 0) {
 		r+=	"<p align=\"center\">\n";
-		r+= "\t<b>Read Transfer @ "+QString::number(dev->tspeeds.rt)+"X</b><br>\n";
+		r+= "\t<b>Read Transfer @ " + QString::number(dev->tspeeds.rt)+"X</b><br>\n";
 		r+= QString("\tTest time: %1:%2<br>\n")
 			.arg( ((int)dev->testData.rt_time) / 60)
 			.arg( ((int)dev->testData.rt_time) % 60, 2, 10, QChar('0'));
@@ -1478,7 +1478,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		r+= "<br>\n";
 		
 		r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-		r+= "<img src=\""+idir+"/rt.png\" alt=\"Read Transfer\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+		r+= "<img src=\"" + idir + "/rt.png\" alt=\"Read Transfer\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 //		r+= "<br>Read Transfer";
 		r+= "</td></tr>\n</table><br>\n";
 	}
@@ -1509,7 +1509,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		r+= "<br>\n";
 
 		r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-		r+= "<img src=\""+idir+"/wt.png\" alt=\"Write Transfer\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+		r+= "<img src=\"" + idir + "/wt.png\" alt=\"Write Transfer\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 //		r+= "<br>Write Transfer";
 		r+= "</td></tr>\n</table><br>\n";
 	}
@@ -1572,7 +1572,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		r+= "<br>\n";
 
 		r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-		r+= "<img src=\""+idir+"/errc.png\" alt=\"Error correction\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">\n";
+		r+= "<img src=\"" + idir + "/errc.png\" alt=\"Error correction\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">\n";
 		r+= "<br>Overall ERRC Graph";
 		r+= "</td></tr></table><br>\n";
 
@@ -1581,7 +1581,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 			for (int i=0; i<8; i++) {
 				if (!dev->media.tdata_errc || (dev->media.tdata_errc & (1<<i))) {
 					r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-					r+= "\t<img src=\""+idir+"/"+errcNameCD[i]+".png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+					r+= "\t<img src=\"" + idir + "/"+errcNameCD[i]+".png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 					r+= "<br>"+errcNameCD[i];
 					r+= "</td></tr>\n</table><br>\n";
 				}
@@ -1590,7 +1590,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 			for (int i=1; i<8; i++) {
 				if (!dev->media.tdata_errc || (dev->media.tdata_errc & (1<<i))) {
 					r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-					r+= "\t<img src=\""+idir+"/"+errcNameDVD[i]+".png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+					r+= "\t<img src=\"" + idir + "/"+errcNameDVD[i]+".png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 					r+= "<br>"+errcNameDVD[i];
 					r+= "</td></tr>\n</table><br>\n";
 				}
@@ -1600,7 +1600,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 			for (int i=0; i<3; i++) {
 				if (!dev->media.tdata_errc || (dev->media.tdata_errc & (1<<residx[i]))) {
 					r+= "<table border=0 cellspacing=0 align=\"center\">\n\t<tr><td align=\"center\">";
-					r+= "\t<img src=\""+idir+"/"+errcNameBD[residx[i]]+".png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+					r+= "\t<img src=\"" + idir + "/"+errcNameBD[residx[i]]+".png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 					r+= "<br>"+errcNameBD[residx[i]];
 					r+= "</td></tr>\n</table><br>\n";
 				}
@@ -1627,7 +1627,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		r+= "\t</table>\n";
 		r+= "<br>\n";
 		r+= "<table border=0 cellspacing=0 align=center>\n\t<tr><td align=center>\n";
-		r+= "<img src=\""+idir+"/jb.png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+		r+= "<img src=\"" + idir + "/jb.png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 		r+= "</td></tr>\n</table><br>\n";
 	}
 
@@ -1644,7 +1644,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 		r+= "\t</table>\n";
 		r+= "<br>\n";
 		r+= "<table border=0 cellspacing=0 align=center>\n\t<tr><td align=center>\n";
-		r+= "<img src=\""+idir+"/ft.png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ">";
+		r+= "<img src=\"" + idir + "/ft.png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ ">";
 		r+= "</td></tr></table><br>\n";
 	}
 
@@ -1664,9 +1664,9 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 			for (int z=0; z<3; z++) 
 			{
 				r+= "<table border=0 cellspacing=0 align=center>\n\t<tr><td align=center>";
-				r+= "<img src=\""+idir+"/ta"+QString::number(l)+"_"+QString::number(z)+"_p.png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ2">";
+				r+= "<img src=\"" + idir + "/ta"+QString::number(l)+"_"+QString::number(z)+"_p.png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ2 ">";
 				r+= "</td></tr>\n\t<tr><td align=\"center\">";
-				r+= "<img src=\""+idir+"/ta"+QString::number(l)+"_"+QString::number(z)+"_l.png\" width="HTML_GRAPH_WQ" height="HTML_GRAPH_HQ2">";
+				r+= "<img src=\"" + idir + "/ta"+QString::number(l)+"_"+QString::number(z)+"_l.png\" width=" HTML_GRAPH_WQ " height=" HTML_GRAPH_HQ2 ">";
 				r+= "</td></tr>\n</table><br>\n";
 			}
 		}
@@ -1675,7 +1675,7 @@ QString QPxToolMW::generate_html(device *dev, QString idir)
 	r+= "\t<br><br>\n";
 	r+= "\t\n";
 	r+=	"<p align=\"center\">\n";
-	r+= "\tGenerated by QPxTool v"VERSION" (c) Gennady \"ShultZ\" Kozlov\n";
+	r+= "\tGenerated by QPxTool v" VERSION " (c) Gennady \"ShultZ\" Kozlov\n";
 	r+= "\t<a href=\"http://qpxtool.sourceforge.net\">http://qpxtool.sourceforge.net</a><br>\n";
 	r+= "</p>\n";
 	r+= "</body>\n</html>\n";
