a0471d4dc758f1a4e176824cb69dc4a3e00983dc
Stéphane Lesimple <speed47_github@speed47.net>
2026-03-28 13:29:19 +0100

    adjust packaging workflows for Qt6

--- .github/workflows/make-dist.sh
+++ .github/workflows/make-dist.sh
@@ -26,8 +26,8 @@ mkdir dist
 for i in $(ntldd -R gui/$flavor/qpxtool.exe  | awk '/mingw/ {print $3}' | tr \\\\ / | grep -Eo '[^/]+$'); do cp -va $MINGW_PREFIX/bin/$i dist/; done
 cp -va gui/$flavor/qpxtool.exe console/*/*.exe dist/
 find lib/lib -name "*.dll" | xargs cp -vat dist/
-mkdir -p dist/share/qt5/plugins/platforms
-cp -va $MINGW_PREFIX/share/qt5/plugins/platforms/* dist/share/qt5/plugins/platforms/
+mkdir -p dist/share/qt6/plugins/platforms
+cp -va $MINGW_PREFIX/share/qt6/plugins/platforms/* dist/share/qt6/plugins/platforms/
 mkdir -p dist/plugins
 cp -va plugins/lib/libqscan_*.dll dist/plugins/
 
